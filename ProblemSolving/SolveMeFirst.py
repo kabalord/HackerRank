089f85a873eba66c64978a0a34d084e561a661fc
walter ROA SERRANO <wroaserrano@gmail.com>
2024-05-23 22:37:13 +0200

    SolveMeFirst

---
+++ ProblemSolving/SolveMeFirst.py
@@ -0,0 +1,13 @@
+# Solve Me First
+
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    
+    return  a + b
+
+num1 = int(input("enter 1 number : "))
+num2 = int(input("enter 2 number : "))    
+res = solveMeFirst(num1,num2)
+print("the sum of these numbers is : ", res)
+
+
