20eca3d289d941c426b42daaa538ae780e9de5c1
walter ROA SERRANO <wroaserrano@gmail.com>
2024-05-29 21:50:45 +0200

    CompareTriplets

---
+++ ProblemSolving/CompareTriplets.py
@@ -0,0 +1,15 @@
+#Compare Triplets
+
+def compareTriplets(a, b):
+    # Write your code here
+    alice=0
+    bob=0
+    
+    for i in range(len(a)):
+        if a[i]> b[i]:
+            alice+=1
+        elif a[i]< b[i]:
+            bob+=1
+    return (alice, bob)
+
+print(compareTriplets([17, 28, 30], [99, 16, 8]))
